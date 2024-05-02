9645ea0dc1574d7a97d950097890c6f18a700795
githubAccountUserLogIn <167630938+githubAccountUserLogIn@users.noreply.github.com>
2024-05-02 13:46:52 +0000

    xssFilter

--- myApp.js
+++ myApp.js
@@ -3,9 +3,8 @@ const app = express();
 
 const helmet = require('helmet');
 app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
-
-app.use(helmet.frameguard({action: 'deny'}))
-
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
 
 
 
